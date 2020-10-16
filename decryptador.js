3e937314021402e6b0dc8fafffc15cf6a749b6c5
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 13:55:54 -0300

    jh

--- decryptador.js
+++ decryptador.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Decryptador
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Joaquin 
 // @match        https://www.google.com/search?q=how+create+script+to+tamper+moenjey+linked+with+github&oq=how+create+script+to+tamper+moenjey+linked+with+github&aqs=chrome..69i57.24511j1j7&sourceid=chrome&ie=UTF-8
 // @grant        none
 // ==/UserScript==
