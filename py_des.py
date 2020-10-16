ead19edd15955f9bcde90c1d649e31040fb934e1
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 13:37:21 -0300

    da

--- py_des.py
+++ py_des.py
@@ -6,12 +6,33 @@ from base64 import b64encode
 #   k = pyDes.des(b"DESCRYPT", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
 # pyDes.des(key, [mode], [IV], [pad], [padmode])
 
-data = b'Please a'
-k = pyDes.des(b'DESCRYPT', pyDes.ECB, "\0\0\0\0\0\0\0\0", pad= None, padmode= pyDes.PAD_NORMAL)
+data = b'Este texto esta cifrado o no?'
+key = b'clavepip'
+k = pyDes.des( key, pyDes.ECB, "\0\0\0\0\0\0\0\0", pad= None, padmode= pyDes.PAD_PKCS5 )
+#crear variables modificables y crear ciclos
 d = k.encrypt(data)
 
 c_b64 = b64encode(d).decode('utf-8')
 
+c_b64_text = '"'+c_b64+'"'
+
 print (c_b64)
 print (k.decrypt(d))
-#assert k.decrypt(d) == data
+
+f = open('index.html','wb')
+
+message = """<html>
+
+    <head>
+        <title>HTML base Tag</title>
+    </head>
+
+    <body>
+        <p>Este sitio contiene un mensaje secreto</p>
+        <div class="DES_ECB" id="""+c_b64_text+"""></div><!DOCTYPE html>
+    </body>
+
+</html>"""
+
+f.write(message.encode('utf-8'))
+f.close()
