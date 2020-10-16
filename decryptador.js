9c44230cda9f18b7427ea0b3eb4cc58d17bf7c18
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 17:30:13 -0300

    k

--- decryptador.js
+++ decryptador.js
@@ -3,14 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.8
 // @description  Decifra el mensaje del index
-// @author       Joaquin 
+// @author       Joaquin
 // @match        https://joak-in-joke.github.io/CriptoTarea3/
 // @grant        none
-// @require      https://raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/crypto-js/4.0.0/mode-ecb.js
-// @require      https://raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/crypto-js/4.0.0/enc-base64.js
-// @require      https://raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/crypto-js/4.0.0/crypto-js.js
-// @require      https://raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/crypto-js/4.0.0/enc-utf8.js
-// ==/UserScript==
+// @require      https://raw.githubusercontent.com/ibrahimelaradi/DES/master/DES/DES.js
+
 (function decrypt() {
     'use strict';
 
@@ -22,4 +19,3 @@
     var d_msg = decrypted.toString(CryptoJS.enc.Utf8);
     console.log(d_msg);
 })();
-
