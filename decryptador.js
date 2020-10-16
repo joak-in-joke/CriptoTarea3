211452f2f93c6afb49e0e71a436dda0bbc28fe26
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 16:04:31 -0300

    khjgjh

--- decryptador.js
+++ decryptador.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Decryptador
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Decifra el mensaje del index
 // @author       Joaquin 
 // @match        https://joak-in-joke.github.io/CriptoTarea3/
