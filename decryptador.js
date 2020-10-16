c8459dd1c800eae6a69946b167ffdf8c29876e7e
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 15:24:50 -0300

    k

--- decryptador.js
+++ decryptador.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name         Decryptador
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Decifra el mensaje del index
 // @author       Joaquin 
 // @match        https://joak-in-joke.github.io/CriptoTarea3/
 // @grant        none
 // ==/UserScript==
+(function decrypt() {
+    'use strict';
 
-(document).ready(function() {
     var msj_enc = document.getElementsByClassName("DES_ECB")[0].id
-    });
+
 
     ('body').append('<div class="DECRYPT" id="HOLA-SOCIO"></div>')
+
+})();
+
