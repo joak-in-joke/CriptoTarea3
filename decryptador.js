c97235e7362fbb565b78cb50f2322fa52d79b72c
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 21:51:59 -0300

    js

--- decryptador.js
+++ decryptador.js
@@ -6,18 +6,18 @@
 // @author       Joaquin
 // @match        https://joak-in-joke.github.io/CriptoTarea3/
 // @grant        none
-// @require      https://raw.githubusercontent.com/ibrahimelaradi/DES/master/DES/DES.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/enc-base64.min.js
 // ==/UserScript==
 
 (function decrypt() {
     'use strict';
 
-    var msj_enc = document.getElementsByClassName("DES_ECB")[0].id;
+    var msj_enc = document.getElementsByClassName("DES")[0].id;
 
     console.log(msj_enc);
 
-    var decrypted = DES.encrypt( "clavepip", msj_enc);
-    var d_msg = decrypted.toString(CryptoJS.enc.Utf8);
-    console.log(d_msg);
+    var decrypted = Crypto.DES.decrypt( msj_enc ,"clavepip");
+    var d_msg = Crypto.dec.Base64.UTF8(decrypted);
+    console.log(d_msg)
 })();
