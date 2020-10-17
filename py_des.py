29a01967d6e3dfcd7c2447d3fb3770ac7dde6f2f
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-10-16 21:44:46 -0300

    limpieza

--- py_des.py
+++ py_des.py
@@ -1,11 +1,6 @@
 import pyDes
 from base64 import b64encode
 
-# For Python3, you'll need to use bytes, i.e.:
-#   data = b"Please encrypt my data"
-#   k = pyDes.des(b"DESCRYPT", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
-# pyDes.des(key, [mode], [IV], [pad], [padmode])
-
 data = b'Este texto esta cifrado o no?'
 key = b'clavepip'
 mod = pyDes.ECB
